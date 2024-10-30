d0ce2f332bca78403afd368d48e11e8786271005
Luca Cavalleri <luca.cavallery@gmail.com>
2024-10-30 23:42:08 +0100

    simplify BITS_TO_REPRESENT

--- src/prefiltering/CacheFriendlyOperations.h
+++ src/prefiltering/CacheFriendlyOperations.h
@@ -3,45 +3,15 @@
 
 #include "IndexTable.h"
 
-#define IS_REPRESENTIBLE_IN_D_BITS(D, N) \
-  (((unsigned long) N >= (1UL << (D - 1)) && (unsigned long) N < (1UL << D)) ? D : -1)
+namespace detail {
+constexpr unsigned long bits_to_represent(const unsigned long N) {
+    if(N==0)return 1;
+    unsigned long result = 31;
+    for(unsigned long D=1;D<=32;++D)result+=((N >= (1UL << (D - 1)) && N < (1UL << D)) ? D : -1);
+    return result;
+};
 
-#define BITS_TO_REPRESENT(N)                            \
-  (N == 0 ? 1 : (31                                     \
-                 + IS_REPRESENTIBLE_IN_D_BITS( 1, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS( 2, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS( 3, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS( 4, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS( 5, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS( 6, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS( 7, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS( 8, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS( 9, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(10, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(11, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(12, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(13, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(14, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(15, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(16, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(17, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(18, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(19, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(20, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(21, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(22, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(23, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(24, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(25, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(26, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(27, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(28, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(29, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(30, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(31, N)    \
-                 + IS_REPRESENTIBLE_IN_D_BITS(32, N)    \
-                 )                                      \
-   )
+}
 
 struct __attribute__((__packed__)) CounterResult {
     unsigned int id;
@@ -69,8 +39,8 @@ template<unsigned int BINSIZE>
 class CacheFriendlyOperations {
 public:
     // 00000000000000000000000111111111
-    static const unsigned int MASK_0_5 = BINSIZE - 1;
-    static const unsigned int MASK_0_5_BIT = BITS_TO_REPRESENT(MASK_0_5);
+    static constexpr unsigned int MASK_0_5 = BINSIZE - 1;
+    static constexpr unsigned int MASK_0_5_BIT = detail::bits_to_represent(MASK_0_5);
 
     CacheFriendlyOperations(size_t maxElement, size_t initBinSize);
     ~CacheFriendlyOperations();
@@ -129,6 +99,4 @@ private:
     size_t keepMaxElement(CounterResult *output);
 };
 
-#undef BITS_TO_REPRESENT
-#undef IS_REPRESENTIBLE_IN_D_BITS
 #endif
